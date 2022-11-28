c3178a5f09c2ba3542661b98a77370825a22e95c
Bharat saini <sainib601@gmail.com>
2022-11-28 21:39:06 +0530

    route 404 fixed

--- app.js
+++ app.js
@@ -30,7 +30,12 @@ app.use("/api/v1/booking",bookRoutes)
 
 //static
 if(process.env.NODE_ENV==='production'){
+    const path = require('path');
+
     app.use(express.static("frontend/build"))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      });
 }
 
 
