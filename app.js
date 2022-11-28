878172c03e6c9dd20f5cc638d7c2a1ad221cc5db
Bharat saini <sainib601@gmail.com>
2022-11-28 21:44:50 +0530

    static html path fixed

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ if(process.env.NODE_ENV==='production'){
 
     app.use(express.static("frontend/build"))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
       });
 }
 
