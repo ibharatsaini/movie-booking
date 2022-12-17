f01ccceee442a0df5fb54112bbb6a304ca726b32
Bharat saini <sainib601@gmail.com>
2022-12-17 13:38:06 +0530

    dd

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ console.log('production running')
 const path = require('path');
 
 app.use(express.static(path.join(__dirname,"./frontend/build")))
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 });
 // }
