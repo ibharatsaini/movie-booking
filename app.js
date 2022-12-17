0e585d4918c88c77cc028512b459d6022b15ee5c
Bharat saini <sainib601@gmail.com>
2022-12-17 09:50:34 +0530

    object null fixed

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if(process.env.NODE_ENV=='production'){
     console.log('production running')
     const path = require('path');
 
-    app.use(express.static(__dirname,"./frontend/build"))
+    app.use(express.static(path.join(__dirname,"./frontend/build")))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'),function (err){
           res.status(500).json({success:false,error:"route not resolved"})
