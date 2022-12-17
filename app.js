41244511c279eead8953701c80ace6af94d7f54d
Bharat saini <sainib601@gmail.com>
2022-12-17 08:38:04 +0530

    build

--- app.js
+++ app.js
@@ -45,14 +45,14 @@ if(process.env.NODE_ENV=='production'){
 //error handling
 app.use(error)
 
-const database = require("./database")
+// const database = require("./database")
 
 
-const PORT  = process.env.PORT  || 8080
-app.listen(PORT,()=>{
-    console.log("Server listening ", PORT)
-    database()
-})
+// const PORT  = process.env.PORT  || 8080
+// app.listen(PORT,()=>{
+//     console.log("Server listening ", PORT)
+//     database()
+// })
 
 
 
